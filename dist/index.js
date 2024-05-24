fd874eeeda3a9ff97f59a40516e3e3ae9012a1df
rian-nzr <riannazar03@gmail.com>
2024-05-24 16:50:43 +0700

    add branch develop

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,12 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const secrets_1 = require("./secrets");
 const app = (0, express_1.default)();
 app.get('/', (req, res) => {
-    res.send('Application korks!jjjj');
+    res.send('Application work!');
     console.log('kk');
 });
-app.listen(3000, () => {
-    console.log('Application started on port 3000!');
-    console.log('apm');
+app.listen(secrets_1.PORT, () => {
+    console.log(`Application started on port ${secrets_1.PORT}!`);
 });
